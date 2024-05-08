6ec0f760cf76138163bbfd916bcbb2e9316dd942
uma13 <umaveerapaneni@gmail.com>
2024-05-08 14:16:44 +0530

    May 6 2023

---
+++ DrucareTas1/src/com/drucare/task/Sentence.java
@@ -0,0 +1,11 @@
+package com.drucare.task;
+
+public class Sentence {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Hey there "
+		+ "I am Data!");
+	}
+
+}
---
+++ DrucareTas1/src/com/drucare/task/SumOfDigits.java
@@ -0,0 +1,9 @@
+package com.drucare.task;
+
+public class SumOfDigits {
+	  public static void main(String[] args) {
+		  
+          System.out.print(2+4+5);
+
+	  }
+}
