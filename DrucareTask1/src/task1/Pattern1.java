f5c0d3380a3464c9ecc5bdef8327fa9d1926e518
uma13 <umaveerapaneni@gmail.com>
2024-05-09 20:01:06 +0530

    May 6 2023

---
+++ DrucareTask1/src/task1/Pattern1.java
@@ -0,0 +1,23 @@
+package task1;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  for (int i = 5; i >= 1; i -= 2) {
+		        for (int j = 1; j <= i; j++) {
+		            System.out.print("*");
+		        }
+		        System.out.println();
+		    }
+		    
+		    // Lower half of the pattern
+		    for (int i = 3; i <= 5; i += 2) {
+		        for (int j = 1; j <= i; j++) {
+		            System.out.print("*");
+		        }
+		        System.out.println();
+		    }
+	}
+
+}
