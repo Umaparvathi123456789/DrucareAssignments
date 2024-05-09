b6567c0ba8c283347e9701bf9cbae75538a61d3a
uma13 <umaveerapaneni@gmail.com>
2024-05-09 18:18:04 +0530

    May 6 2023

---
+++ DrucareTask2/src/task1/SquareOfDecimal.java
@@ -0,0 +1,11 @@
+package task1;
+
+public class SquareOfDecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float a=3.9f;
+		System.out.println(a*a);
+	}
+
+}
