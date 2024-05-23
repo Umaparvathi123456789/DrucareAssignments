a715e765afed9a8196199e694b2ed484cd10c3e4
uma13 <umaveerapaneni@gmail.com>
2024-05-23 14:56:53 +0530

    may 23

---
+++ Drucare/src/java8/JavaStreams.java
@@ -0,0 +1,69 @@
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JavaStreams {
+
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("Sam", "Peter","ballon","Sam");
+
+		long count=names.stream()
+				   .filter(data->data.length()>3)
+				   .count();
+		System.out.println("length greater than 3 : "+count);
+
+		List<String> namesUpper=names.stream()
+				                .map(String::toUpperCase)
+				                .collect(Collectors.toList());
+		System.out.println(namesUpper);
+
+
+		List<String> cityList = Arrays.asList("chennai", "mumbai", "bangalore");
+
+		List<String> cityUpper=cityList.stream()
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+		System.out.println(cityUpper);
+
+		List<String> citycount=cityList.stream()
+				   .filter(data->data.length()>6)
+				   .collect(Collectors.toList());
+		System.out.println(" length is greaterthan 6 :"+citycount);
+
+		List<String> cityStartWithC=cityList.stream()
+				                    .filter(name->name.startsWith("c"))
+				                    .collect(Collectors.toList());
+		System.out.println("Cities start with c :"+cityStartWithC);
+
+		
+		List<String> cityContainsAI=cityList.stream()
+				                    .filter(city->city.contains("ai"))
+				                    .collect(Collectors.toList());
+		System.out.println("Cities containing ai :"+cityContainsAI);
+
+		List<String> appendMetro=cityList.stream()
+				                 .map(city->city.concat("metro"))
+				                 .collect(Collectors.toList());
+		System.out.println("appending metro :" +appendMetro);
+
+		
+		long cityCountStartWithC=cityList.stream()
+                .filter(name->name.startsWith("c"))
+                .count();
+        System.out.println("Cities start with c :"+cityCountStartWithC);
+
+        long emptyString=cityList.stream()
+        		                 .filter(String::isEmpty)
+        		                 .count();
+		System.out.println("count of empty string :" +emptyString);
+
+		List<String> removeEmptyString=cityList.stream()
+                .filter(city->city.length()!=0)
+                .collect(Collectors.toList());
+        System.out.println("remove of empty string :" +removeEmptyString);
+
+
+	}
+}
