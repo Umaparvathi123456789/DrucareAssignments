1db31400da82c0b3b95b80e95ea40afe0fd36380
uma parvathi veerapaneni <118106779+Umaparvathi123456789@users.noreply.github.com>
2024-07-05 14:28:36 +0530

    Delete E-CommerceOrder directory
    fsf

--- E-CommerceOrder/src/main/java/com/uma/login/repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.uma.login.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.uma.login.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
--- E-CommerceOrder/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-
-Copy code
-CREATE TABLE Order_Table (
-    orderId BIGINT AUTO_INCREMENT PRIMARY KEY,
-    date VARCHAR(255) NOT NULL UNIQUE,
-    total DOUBLE UNIQUE,
-    productId BIGINT
-);
